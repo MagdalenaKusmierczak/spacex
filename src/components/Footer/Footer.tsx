bcfcd0a6b35000cc3aee6b501648f0f4e5c93c6d
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-09-09 11:34:14 +0200

    Creating base component
    Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        Created by
+        <a
+          href="https://github.com/MagdalenaKusmierczak"
+          rel="noopener"
+          target="_blank"
+        >
+          Magdalena Kuśmierczak
+        </a>
+      </p>
+    </div>
+  );
+};
+export default Footer;
