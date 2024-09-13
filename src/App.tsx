3bf5ca9593e74eda15f48e060066809929fea419
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-09-13 09:57:01 +0200

    added routing paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React, { lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Sharedlayout from "./components/Sharedlayout/Sharedlayout";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Sharedlayout />}>
+        {/* <Route index element={<HomePage />} />
+        <Route path="/rocket/:id" element={<RocketPage />}> */}
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
