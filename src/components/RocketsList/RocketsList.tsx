9bf07550e8bc4a05cc302c8e52ddf14d886b0902
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-09-24 11:30:59 +0200

    Update RocketsList.tsx
    added tittle and randomized selection of pictures

--- src/components/RocketsList/RocketsList.tsx
+++ src/components/RocketsList/RocketsList.tsx
@@ -2,10 +2,23 @@ import { FC } from "react";
 import Rocket from "../../service/Interfaces/RocketInterface";
 
 const RocketsList: FC<{ rockets: Rocket[] }> = ({ rockets }) => {
+  //Randomizng selection of images
+  const randomIndex = (arr: Array<string>) => {
+    const arrLength = arr.length;
+    const randomizedIndex = Math.floor(Math.random() * arrLength);
+    return randomizedIndex;
+  };
+
   return (
     <ul>
       {rockets.map((rocket: Rocket) => (
-        <li key={rocket.id}></li>
+        <li key={rocket.id}>
+          <h2>{rocket.rocket_name}</h2>
+          <img
+            alt={rocket.rocket_name}
+            src={rocket.flickr_images[randomIndex(rocket.flickr_images)]}
+          />
+        </li>
       ))}
     </ul>
   );
