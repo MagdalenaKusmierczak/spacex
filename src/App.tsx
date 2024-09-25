438d5b471aba596387e6dfdbe8fd8b152e636efc
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-09-25 10:19:09 +0200

    Update App.tsx
    added RocktPage routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Sharedlayout from "./components/Sharedlayout/Sharedlayout";
 import HomePage from "./pages/Home/HomePage";
+import RocketPage from "./pages/Rocket/RocketPage";
 import "./App.css";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Sharedlayout />}>
         <Route index element={<HomePage />} />
-        {/* <Route path="/rocket/:id" element={<RocketPage />}> */}
+        <Route path="/rocket/:id" element={<RocketPage />} />
       </Route>
     </Routes>
   );
