76596f0ce032703641aa5f42b3e78bf79b8cd1eb
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-09-25 17:27:03 +0200

    Update RocketPage.tsx
    Adding section to page

--- src/pages/Rocket/RocketPage.tsx
+++ src/pages/Rocket/RocketPage.tsx
@@ -1,24 +1,25 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getRocket } from "../../service/API/RocketsAPI";
-
+import RocketSection from "../../components/RocketSection/RocketSection";
 import Rocket from "../../service/Interfaces/RocketInterface";
 
 const RocketPage = () => {
-  const [rocketData, setRocketData] = useState<Rocket[]>([]);
+  const [rocketData, setRocketData] = useState<Rocket | null>(null);
   const { id } = useParams();
   useEffect(() => {
     const fetchData = async () => {
       try {
         const rocket = await getRocket(id as string);
-        console.log(rocket);
         setRocketData(rocket);
+       
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
   }, [id]);
-  return <div>Hello</div>;
+   
+  return rocketData? <RocketSection rocketData={rocketData} />:null;
 };
 export default RocketPage;
