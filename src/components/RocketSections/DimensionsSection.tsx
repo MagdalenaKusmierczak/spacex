01607431b5a16e194f3e469287c5739027bca065
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-09-26 16:53:55 +0200

    Update DimensionsSection.tsx
    fixing paragraphs

--- src/components/RocketSections/DimensionsSection.tsx
+++ src/components/RocketSections/DimensionsSection.tsx
@@ -5,24 +5,24 @@ import setParagraph from "../../utils/setParagraph";
 const DimensionsSection: FC<{ rocketData: Rocket }> = (props) => {
   const rocket = props.rocketData;
   return (
-      <section>
-        <h3>Dimensions</h3>
-        <p>Height</p>
-        <ul>
-          <li></li>
-          <li>{setParagraph("Feet:", rocket.height.feet)}</li>
-        </ul>
-        <p>Diameter</p>
-        <ul>
-          <li>{setParagraph("Meters", rocket.diameter.meters)}</li>
-          <li>{setParagraph("Feet:", rocket.diameter.feet)}</li>
-        </ul>
-        <p>Mass</p>
-        <ul>
-          <li>{setParagraph("Kilograms", rocket.mass.kg)}</li>
-          <li>{setParagraph("Pounds", rocket.mass.lb)}</li>
-        </ul>
-      </section>
+    <section>
+      <h3>Dimensions</h3>
+      <p>Height</p>
+      <ul>
+        <li>{setParagraph("Meter", rocket.height.meters)}</li>
+        <li>{setParagraph("Feet", rocket.height.feet)}</li>
+      </ul>
+      <p>Diameter</p>
+      <ul>
+        <li>{setParagraph("Meters", rocket.diameter.meters)}</li>
+        <li>{setParagraph("Feet", rocket.diameter.feet)}</li>
+      </ul>
+      <p>Mass</p>
+      <ul>
+        <li>{setParagraph("Kilograms", rocket.mass.kg)}</li>
+        <li>{setParagraph("Pounds", rocket.mass.lb)}</li>
+      </ul>
+    </section>
   );
 };
 
