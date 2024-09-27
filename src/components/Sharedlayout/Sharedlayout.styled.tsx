095aca4c3f61129eaa33e0dd93c8d14ea24de261
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-09-27 16:04:23 +0200

    Create Sharedlayout.styled.tsx
    Added based container styles for layout

---
+++ src/components/Sharedlayout/Sharedlayout.styled.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  z-index: 1;
+  position: relative;
+  margin: 0 auto;
+  padding: 30px;
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 1px 3px 21px 11px rgba(50, 50, 50, 0.3);
+  background-color: rgba(50, 50, 50, 0.3);
+  overflow: hidden;
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
+  @media screen and (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
