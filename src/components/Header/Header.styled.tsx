06224416a8f05ec220782d946e011639dc774243
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-09-30 11:20:50 +0200

    Update Header.styled.tsx
    change layout

--- src/components/Header/Header.styled.tsx
+++ src/components/Header/Header.styled.tsx
@@ -2,16 +2,14 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   display: flex;
-  justify-content: space-between;
-  flex-direction: column;
+  justify-content: space-evenly;
   gap: 30px;
   align-items: center;
   margin-bottom: 30px;
   background-color: rgba(80, 103, 103, 0.5);
   box-shadow: 0px 0px 15px 20px rgba(80, 103, 103, 0.5);
-  border: none;
   @media screen and (min-width: 768px) {
-    flex-direction: row;
+    justify-content: space-between;
     gap: 0px;
   }
 `;
