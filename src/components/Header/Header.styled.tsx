ae5f1cb544199c3a42e2d37cb084252a5a9a028a
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-01 09:55:08 +0200

    Update Header.styled.tsx
    changed semi-through for solid color background

--- src/components/Header/Header.styled.tsx
+++ src/components/Header/Header.styled.tsx
@@ -2,12 +2,13 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   display: flex;
+  width:100%;
   justify-content: space-evenly;
   gap: 30px;
   align-items: center;
   margin-bottom: 30px;
-  background-color: rgba(80, 103, 103, 0.5);
-  box-shadow: 0px 0px 15px 20px rgba(80, 103, 103, 0.5);
+  background-color: rgba(80, 103, 103, 1);
+  box-shadow: 0px 0px 15px 20px rgba(80, 103, 103, 1);
   @media screen and (min-width: 768px) {
     justify-content: space-between;
     gap: 0px;
