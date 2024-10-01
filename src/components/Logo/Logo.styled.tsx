a79d6eeb031bdcfba3fccf07412eb1438190bcdc
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-01 11:02:00 +0200

    Update Logo.styled.tsx
    added animation

--- src/components/Logo/Logo.styled.tsx
+++ src/components/Logo/Logo.styled.tsx
@@ -9,8 +9,13 @@ export const ImageWrapper = styled.div`
   border-radius: 50px;
   background-color: #f0ff3836;
   box-shadow: 0px 0px 8px 8px #f0ff3836;
-  &:hover {
+  border-radius: 50%;
+  transition: all 0.3s ease-in-out 0s;
+    &:hover {
+      transform: rotate(360deg);
+      transition: all 0.3s ease-in-out 0s;
     background-color: #ffc41f;
-    box-shadow: 0px 0px 8px 8px #fada7e;
+    box-shadow: 0px 0px 8px 8px #fada7e;}
+    
   }
 `;
