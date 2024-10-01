cee87de1ffb954f5ac09c7f31d4439ec25291610
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-01 15:47:14 +0200

    Update Navigation.tsx
    change order

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,8 +3,8 @@ import { NavItem, NavWrapper } from "./Navigation.styled";
 const Navigation = () => {
   return (
     <NavWrapper>
-      <NavItem to="/rocket/falcon9">Falcon 9</NavItem>
       <NavItem to="/rocket/falcon1">Falcon 1</NavItem>
+      <NavItem to="/rocket/falcon9">Falcon 9</NavItem>
       <NavItem to="/rocket/falconheavy">Falcon Heavy</NavItem>
       <NavItem to="/rocket/starship">Starship</NavItem>
     </NavWrapper>
