9edb67c907c053d4b54d9723c6cd85f386ef816e
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-02 12:52:47 +0200

    General Information section
    base styles and icon

--- src/components/RocketSections/GeneralSection.tsx
+++ src/components/RocketSections/GeneralSection.tsx
@@ -1,21 +1,26 @@
 import { FC } from "react";
 import Rocket from "../../service/Interfaces/RocketInterface";
 import setParagraph from "../../utils/setParagraph";
+import { SecondHeader,Summary } from "./RocketSections.styled";
 
 const GeneralSection: FC<{ rocketData: Rocket }> = (props) => {
   const rocket = props.rocketData;
   return (
     <section>
-      <h3>General information</h3>
-      {setParagraph("Rocket type", rocket.rocket_type)}
-      {setParagraph("Active", rocket.active)}
-      {setParagraph("Number of stages", rocket.stages)}
-      {setParagraph("Number of boosters", rocket.boosters)}
-      {setParagraph("Cost per launch", rocket.cost_per_launch)}
-      {setParagraph("Success rate", rocket.success_rate_pct)}
-      {setParagraph("First flight", rocket.first_flight)}
-      {setParagraph("Country", rocket.country)}
-      {setParagraph("Company", rocket.company)}
+      <details>
+        <Summary>
+          <SecondHeader>General information</SecondHeader>
+        </Summary>
+        {setParagraph("Rocket type", rocket.rocket_type)}
+        {setParagraph("Active", (rocket.active).toString())}
+        {setParagraph("Number of stages", rocket.stages)}
+        {setParagraph("Number of boosters", rocket.boosters)}
+        {setParagraph("Cost per launch", rocket.cost_per_launch)}
+        {setParagraph("Success rate", rocket.success_rate_pct)}
+        {setParagraph("First flight", rocket.first_flight)}
+        {setParagraph("Country", rocket.country)}
+        {setParagraph("Company", rocket.company)}
+      </details>
     </section>
   );
 };
--- src/components/RocketSections/RocketSections.styled.tsx
+++ src/components/RocketSections/RocketSections.styled.tsx
@@ -1,9 +1,30 @@
 import styled from "styled-components";
+import arrow from "../../assets/down-arrow.png";
+//Wrappers
 export const IntroWrapper = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
+export const Summary = styled.summary`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:after {
+    content: "";
+    width: 20px;
+    height: 18px;
+    background: url(${arrow}) no-repeat;
+    background-size: contain;
+    margin-left: 0.75em;
+    transition: 0.2s;
+  }
+  &:-webkit-details-marker {
+    display: none;
+  }
+`;
+//Headers
 export const FirstHeader = styled.h1`
   margin-block: 1em;
   text-align: center;
@@ -12,9 +33,22 @@ export const FirstHeader = styled.h1`
   @media screen and (min-width: 768px) {
     font-size: 24px;
   }
+`;
+export const SecondHeader = styled.h2`
+  display: inline;
+  margin-block: 1em;
+  text-align: center;
+  font-size: 18px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
   @media screen and (min-width: 1280px) {
+    font-size: 22px;
   }
 `;
+
+//Paragraphs
 export const Description = styled.p`
   margin-top: 0;
   margin-bottom: 1em;
@@ -25,6 +59,7 @@ export const Description = styled.p`
     font-size: 20px;
   }
   @media screen and (min-width: 1280px) {
+    font-size: 22px;
     width: 65%;
   }
 `;
