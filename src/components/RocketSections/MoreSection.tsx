af8c156ee879d15cc89a3743723464d134c4934f
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-04 11:58:11 +0200

    Update MoreSection.tsx

--- src/components/RocketSections/MoreSection.tsx
+++ src/components/RocketSections/MoreSection.tsx
@@ -1,17 +1,16 @@
 import { FC } from "react";
 import Rocket from "../../service/Interfaces/RocketInterface";
-import { SecondHeader, Summary } from "./RocketSections.styled";
 
 const MoreSection: FC<{ rocketData: Rocket }> = (props) => {
   const rocket = props.rocketData;
   return (
-    <section><details><Summary><SecondHeader>More</SecondHeader></Summary>
+    <section id="more">
       <p>
         For more information go to:
         <a href={rocket.wikipedia} rel="noopener" target="_blank">
           Wikipedia {rocket.rocket_name} page.
         </a>
-      </p></details>
+      </p>
     </section>
   );
 };
