897913b3e05280a303ca2342f85d938171827968
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-04 16:21:21 +0200

    Added menu
    Added unfolding menu and styles

--- src/components/ButtonsList/ButtonsList.styled.tsx
+++ src/components/ButtonsList/ButtonsList.styled.tsx
@@ -2,26 +2,28 @@ import styled from "styled-components";
 import arrow from "../../assets/down-arrow.png";
 
 export const Button = styled.button`
+  position: relative;
   background: none;
   color: inherit;
+  padding: 0;
   border: none;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
-  &:after {
-    content: "";
-    width: 20px;
-    height: 18px;
-    background: url(${arrow}) no-repeat;
-    background-size: contain;
-    margin-left: 0.75em;
-    transition: 0.2s;
+  border-color: #ffc41f;
+  transition: all 150ms ease-in-out;
+  border-radius: 10px;
+  box-shadow: 0 0 10px 0 #ffc41f inset, 0 0 10px 4px #ffc41f;
+  &:hover {
+    box-shadow: 0 0 40px 40px #29373ce6 inset, 0 0 0 0 #29373ce6;
   }
 `;
+export const MenuButton = styled(Button)`
+  text-transform: capitalize;
+`;
 export const SectionName = styled.span`
-  display: inline;
-  margin-block: 1em;
+  margin: 10px;
   text-align: center;
   font-size: 18px;
 
@@ -33,11 +35,20 @@ export const SectionName = styled.span`
   }
 `;
 export const ButtonsWrapper = styled.div`
+  margin-block: 10px;
+  margin-right: 10px;
   display: flex;
   flex-direction: column;
+  gap: 10px;
   @media screen and (min-width: 768px) {
+    margin-top: 20px;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
   }
+`;
+export const MenuWrapper = styled.div`
+  margin-top: 15px;
+  display: flex;
+  justify-content: center;
 `;
--- src/components/ButtonsList/ButtonsList.tsx
+++ src/components/ButtonsList/ButtonsList.tsx
@@ -1,39 +1,60 @@
-import { Button, SectionName, ButtonsWrapper } from "./ButtonsList.styled";
+import {
+  Button,
+  MenuButton,
+  SectionName,
+  ButtonsWrapper,
+  MenuWrapper,
+} from "./ButtonsList.styled";
+import { useState } from "react";
 
 interface ButtonsListProps {
   handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  activeSection: string;
 }
 
-const ButtonsList = ({ handleClick }: ButtonsListProps) => {
+const ButtonsList = ({ handleClick, activeSection }: ButtonsListProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <>
-      <ButtonsWrapper>
-        <Button data-target="gallery" onClick={handleClick}>
-          <SectionName>Gallery</SectionName>
-        </Button>
-        <Button data-target="general-info" onClick={handleClick}>
-          <SectionName>General information</SectionName>
-        </Button>
-        <Button data-target="dimnensions" onClick={handleClick}>
-          <SectionName>Dimensions</SectionName>
-        </Button>
-        <Button data-target="engines" onClick={handleClick}>
-          <SectionName>Engines</SectionName>
-        </Button>
-        <Button data-target="landing-legs" onClick={handleClick}>
-          <SectionName>Landing legs</SectionName>
-        </Button>
-        <Button data-target="payloads" onClick={handleClick}>
-          <SectionName>Payloads</SectionName>
-        </Button>
-        <Button data-target="stages" onClick={handleClick}>
-          <SectionName>Stages</SectionName>
-        </Button>
-        <Button data-target="more" onClick={handleClick}>
-          <SectionName>More</SectionName>
-        </Button>
-      </ButtonsWrapper>
-    </>
+    <MenuWrapper>
+      <MenuButton data-target="details" onClick={handleToggle}>
+        <SectionName>
+          {activeSection ? "Details" + ":" + " " + activeSection : "Details"}
+        </SectionName>
+        {isOpen ? (
+          <ButtonsWrapper>
+            <Button data-target="gallery" onClick={handleClick}>
+              <SectionName>Gallery</SectionName>
+            </Button>
+            <Button data-target="general information" onClick={handleClick}>
+              <SectionName>General information</SectionName>
+            </Button>
+            <Button data-target="dimensions" onClick={handleClick}>
+              <SectionName>Dimensions</SectionName>
+            </Button>
+            <Button data-target="engines" onClick={handleClick}>
+              <SectionName>Engines</SectionName>
+            </Button>
+            <Button data-target="landing legs" onClick={handleClick}>
+              <SectionName>Landing legs</SectionName>
+            </Button>
+            <Button data-target="payloads" onClick={handleClick}>
+              <SectionName>Payloads</SectionName>
+            </Button>
+            <Button data-target="stages" onClick={handleClick}>
+              <SectionName>Stages</SectionName>
+            </Button>
+            <Button data-target="more" onClick={handleClick}>
+              <SectionName>More</SectionName>
+            </Button>
+          </ButtonsWrapper>
+        ) : null}
+      </MenuButton>
+    </MenuWrapper>
   );
 };
 
