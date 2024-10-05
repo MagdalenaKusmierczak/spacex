edf7a2a0cfc04f618ab6dac7026f5b9db0152924
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-05 14:14:16 +0200

    Update DimensionsSection.tsx
    Begging section stylization

--- src/components/RocketSections/DimensionsSection.tsx
+++ src/components/RocketSections/DimensionsSection.tsx
@@ -1,27 +1,33 @@
 import { FC } from "react";
 import Rocket from "../../service/Interfaces/RocketInterface";
 import setParagraph from "../../utils/setParagraph";
+import {
+  SectionWrapper,
+  SecondHeader,
+  List,
+  ListItem,
+} from "./RocketSections.styled";
 
 const DimensionsSection: FC<{ rocketData: Rocket }> = (props) => {
   const rocket = props.rocketData;
   return (
-    <section id="dimensions">
-      <p>Height</p>
-      <ul>
-        <li>{setParagraph("Meter", rocket.height.meters)}</li>
-        <li>{setParagraph("Feet", rocket.height.feet)}</li>
-      </ul>
-      <p>Diameter</p>
-      <ul>
-        <li>{setParagraph("Meters", rocket.diameter.meters)}</li>
-        <li>{setParagraph("Feet", rocket.diameter.feet)}</li>
-      </ul>
-      <p>Mass</p>
-      <ul>
-        <li>{setParagraph("Kilograms", rocket.mass.kg)}</li>
-        <li>{setParagraph("Pounds", rocket.mass.lb)}</li>
-      </ul>
-    </section>
+    <SectionWrapper id="dimensions">
+      <SecondHeader>Height</SecondHeader>
+      <List>
+        <ListItem>{setParagraph("Meter", rocket.height.meters)}</ListItem>
+        <ListItem>{setParagraph("Feet", rocket.height.feet)}</ListItem>
+      </List>
+      <SecondHeader>Diameter</SecondHeader>
+      <List>
+        <ListItem>{setParagraph("Meters", rocket.diameter.meters)}</ListItem>
+        <ListItem>{setParagraph("Feet", rocket.diameter.feet)}</ListItem>
+      </List>
+      <SecondHeader>Mass</SecondHeader>
+      <List>
+        <ListItem>{setParagraph("Kilograms", rocket.mass.kg)}</ListItem>
+        <ListItem>{setParagraph("Pounds", rocket.mass.lb)}</ListItem>
+      </List>
+    </SectionWrapper>
   );
 };
 
