6b12287b1ddb0147138deb7a75c7d7749ba3465d
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-07 11:12:31 +0200

    Update GeneralSection.tsx
    change order

--- src/components/RocketSections/GeneralSection.tsx
+++ src/components/RocketSections/GeneralSection.tsx
@@ -1,21 +1,23 @@
 import { FC } from "react";
 import Rocket from "../../service/Interfaces/RocketInterface";
 import setParagraph from "../../utils/setParagraph";
+import { SectionWrapper } from "./RocketSections.styled";
+
 
 const GeneralSection: FC<{ rocketData: Rocket }> = (props) => {
   const rocket = props.rocketData;
   return (
-    <section id="general information">
+    <SectionWrapper id="general information">
       {setParagraph("Rocket type", rocket.rocket_type)}
-      {setParagraph("Active", rocket.active.toString())}
+      {setParagraph("First flight", rocket.first_flight)}
+      {setParagraph("Company", rocket.company)}
+      {setParagraph("Country", rocket.country)}
       {setParagraph("Number of stages", rocket.stages)}
       {setParagraph("Number of boosters", rocket.boosters)}
       {setParagraph("Cost per launch", rocket.cost_per_launch)}
       {setParagraph("Success rate", rocket.success_rate_pct)}
-      {setParagraph("First flight", rocket.first_flight)}
-      {setParagraph("Country", rocket.country)}
-      {setParagraph("Company", rocket.company)}
-    </section>
+      {setParagraph("Active", rocket.active.toString())}
+    </SectionWrapper>
   );
 };
 
