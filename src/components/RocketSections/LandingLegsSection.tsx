b48d71177b642cacd96e35d5672e5c24682d922b
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-07 11:40:23 +0200

    Update LandingLegsSection.tsx
    apply styles

--- src/components/RocketSections/LandingLegsSection.tsx
+++ src/components/RocketSections/LandingLegsSection.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import Rocket from "../../service/Interfaces/RocketInterface";
 import setParagraph from "../../utils/setParagraph";
+import { SectionWrapper } from "./RocketSections.styled";
 
 const LandingLegsSection: FC<{ rocketData: Rocket }> = (props) => {
   const rocket = props.rocketData;
   console.log(rocket);
   return (
-    <section id="landing legs">
+    <SectionWrapper id="landing legs">
       {setParagraph("Number of legs", rocket.landing_legs.number)}
       {setParagraph(
         "Material",
@@ -14,7 +15,7 @@ const LandingLegsSection: FC<{ rocketData: Rocket }> = (props) => {
           ? rocket.landing_legs.material
           : "Unknown"
       )}
-    </section>
+    </SectionWrapper>
   );
 };
 
