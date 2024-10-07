d12becaf0cbae6da94fe86a1e9afe14e8bffbf54
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-07 13:19:36 +0200

    Update StagesSection.tsx
    apply styles

--- src/components/RocketSections/StagesSection.tsx
+++ src/components/RocketSections/StagesSection.tsx
@@ -1,15 +1,16 @@
 import { FC } from "react";
 import Rocket from "../../service/Interfaces/RocketInterface";
 import setParagraph from "../../utils/setParagraph";
+import { SectionWrapper,StagesList,StagesItem, SecondHeader,ThirdHeader,OutsideList, StyledParagraph, StyledSpan } from "./RocketSections.styled";
 
 const StagesSection: FC<{ rocketData: Rocket }> = (props) => {
   const rocket = props.rocketData;
   return (
-    <section id="stages">
-      <ul>
-        <li>
-          <h4>First stage</h4>
-          {setParagraph("Reusable", rocket.first_stage.reusable)}
+    <SectionWrapper id="stages">
+      <StagesList>
+        <StagesItem>
+          <SecondHeader>First stage</SecondHeader>
+          {setParagraph("Reusable", rocket.first_stage.reusable.toString())}
           {setParagraph("Number of engines", rocket.first_stage.engines)}
           {setParagraph(
             "Fuel amount",
@@ -21,8 +22,8 @@ const StagesSection: FC<{ rocketData: Rocket }> = (props) => {
             rocket.first_stage.burn_time_sec,
             "seconds"
           )}
-          <h5>Thrust sea level</h5>
-          <ul>
+          <ThirdHeader>Thrust sea level</ThirdHeader>
+          <OutsideList>
             <li>
               {setParagraph(
                 "Kilonewtons",
@@ -37,9 +38,9 @@ const StagesSection: FC<{ rocketData: Rocket }> = (props) => {
                 "lbf"
               )}
             </li>
-          </ul>
-          <h5>Thrust vacuum</h5>
-          <ul>
+          </OutsideList>
+          <ThirdHeader>Thrust vacuum</ThirdHeader>
+          <OutsideList>
             <li>
               {setParagraph(
                 "Kilonewtons",
@@ -54,10 +55,10 @@ const StagesSection: FC<{ rocketData: Rocket }> = (props) => {
                 "lbf"
               )}
             </li>
-          </ul>
-        </li>
-        <li>
-          <h4>Second stage</h4>
+          </OutsideList>
+        </StagesItem>
+        <StagesItem>
+          <SecondHeader>Second stage</SecondHeader>
           {setParagraph("Number of engines", rocket.second_stage.engines)}
           {setParagraph(
             "Fuel amount",
@@ -69,10 +70,9 @@ const StagesSection: FC<{ rocketData: Rocket }> = (props) => {
             rocket.second_stage.burn_time_sec,
             "seconds"
           )}
-          <h5>Thrust</h5>
-          <ul>
+          <ThirdHeader>Thrust</ThirdHeader>
+          <OutsideList>
             <li>
-              {" "}
               {setParagraph("Kilonewtons", rocket.second_stage.thrust.kN, "kN")}
             </li>
             <li>
@@ -82,8 +82,8 @@ const StagesSection: FC<{ rocketData: Rocket }> = (props) => {
                 "lbf"
               )}
             </li>
-          </ul>
-          <h5>Payloads</h5>
+          </OutsideList>
+          <ThirdHeader>Payloads</ThirdHeader>
           {setParagraph("First option", rocket.second_stage.payloads.option_1)}
           {rocket.second_stage.payloads.option_2
             ? setParagraph(
@@ -91,9 +91,9 @@ const StagesSection: FC<{ rocketData: Rocket }> = (props) => {
                 rocket.second_stage.payloads.option_2
               )
             : null}
-          <h6>Composite fairing</h6>
-          <p>Height</p>
-          <ul>
+          <ThirdHeader>Composite fairing</ThirdHeader>
+          <StyledParagraph>Height</StyledParagraph>
+          <OutsideList>
             <li>
               {setParagraph(
                 "Meters",
@@ -106,9 +106,9 @@ const StagesSection: FC<{ rocketData: Rocket }> = (props) => {
                 rocket.second_stage.payloads.composite_fairing.height.feet
               )}
             </li>
-          </ul>
-          <p>Diamater</p>
-          <ul>
+          </OutsideList>
+          <StyledParagraph>Diamater</StyledParagraph>
+          <OutsideList>
             <li>
               {setParagraph(
                 "Meters",
@@ -121,10 +121,10 @@ const StagesSection: FC<{ rocketData: Rocket }> = (props) => {
                 rocket.second_stage.payloads.composite_fairing.diameter.feet
               )}
             </li>
-          </ul>
-        </li>
-      </ul>
-    </section>
+          </OutsideList>
+        </StagesItem>
+      </StagesList>
+    </SectionWrapper>
   );
 };
 
