8593e324c1049ccfa7f88725d0697aabe009a646
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-08 12:37:25 +0200

    Gallery finish
    added lightbox on imageclick

--- src/components/RocketSections/GallerySection.tsx
+++ src/components/RocketSections/GallerySection.tsx
@@ -1,17 +1,26 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Rocket from "../../service/Interfaces/RocketInterface";
+import { SectionWrapper, ImageWrapper, Image } from "./RocketSections.styled";
+//Carousel imports
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { SectionWrapper, ImageWrapper, Image } from "./RocketSections.styled";
 import PrevArrow from "../GalleryArrows/PrevArrow";
 import NextArrow from "../GalleryArrows/NextArrow";
+//Lightbox imports
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+interface Slide {
+  src: string;
+  alt: string;
+}
 
 const GallerySection: FC<{ rocketData: Rocket }> = (props) => {
   const rocket = props.rocketData;
   // Carousel settings
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
@@ -22,16 +31,47 @@ const GallerySection: FC<{ rocketData: Rocket }> = (props) => {
     prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />,
   };
+  //Lightbox settings
+  const [open, setOpen] = useState(false);
+  const [slides, setSlides] = useState<Slide[]>([]);
+
+  const handleImageClick = (image: string) => {
+    const slide: Slide = {
+      src: image,
+      alt: rocket.rocket_name,
+    };
+    setSlides([slide]);
+    setOpen(true);
+  };
 
   return (
     <SectionWrapper id="gallery">
       <Slider {...settings}>
         {rocket.flickr_images.map((image) => (
           <ImageWrapper key={rocket.flickr_images.indexOf(image)}>
-            <Image src={image} alt={rocket.rocket_name} />
+            <Image
+              src={image}
+              alt={rocket.rocket_name}
+              onClick={() => handleImageClick(image)}
+            />
           </ImageWrapper>
         ))}
       </Slider>
+      {open && (
+        <Lightbox
+          carousel={{ finite: slides.length <= 1 }}
+          render={{
+            buttonPrev: slides.length <= 1 ? () => null : undefined,
+            buttonNext: slides.length <= 1 ? () => null : undefined,
+          }}
+          controller={{
+            closeOnBackdropClick: true,
+          }}
+          open={open}
+          close={() => setOpen(false)}
+          slides={slides}
+        />
+      )}
     </SectionWrapper>
   );
 };
