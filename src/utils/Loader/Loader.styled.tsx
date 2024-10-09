26fde021ca6a8f3d70af87149d243f9e12bc30d6
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-09 09:52:06 +0200

    Update Loader.styled.tsx
    Style finish

--- src/utils/Loader/Loader.styled.tsx
+++ src/utils/Loader/Loader.styled.tsx
@@ -6,5 +6,12 @@ export const LoaderWrapper = styled.div`
   min-height: 90vh;
   display: flex;
   justify-content: center;
-  margin-top: 30vh;
+ margin-top:20vh;
+  @media screen and (min-width: 768px) {
+ margin-top: 25vh;
+    }
+  }
+  @media screen and (min-width: 1280px) {
+ margin-top: 30vh; 
+  }
 `;
