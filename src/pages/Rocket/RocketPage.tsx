28b44e2203b1bf574cfa5bce5f3243fbcdc7b497
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-12 09:20:06 +0200

    clearing code RocketPage
    imports order,
    delete depricated imports,
    code order

--- src/pages/Rocket/RocketPage.tsx
+++ src/pages/Rocket/RocketPage.tsx
@@ -1,22 +1,15 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getRocket } from "../../service/API/RocketsAPI";
-import Rocket from "../../service/Interfaces/RocketInterface";
 import IntroSection from "../../components/RocketSections/IntroSection";
-import GeneralSection from "../../components/RocketSections/GeneralSection";
-import DimensionsSection from "../../components/RocketSections/DimensionsSection";
-import StagesSection from "../../components/RocketSections/StagesSection";
-import EnginesSection from "../../components/RocketSections/EnginesSection";
-import GallerySection from "../../components/RocketSections/GallerySection";
-import PayloadsSection from "../../components/RocketSections/PayloadsSection";
-import LandingLegsSection from "../../components/RocketSections/LandingLegsSection";
-import MoreSection from "../../components/RocketSections/MoreSection";
-import { Main } from "../Main.styled";
 import RocketDetails from "../../components/RocketDetails/RocketDetails";
+import Rocket from "../../service/Interfaces/RocketInterface";
+import { Main } from "../Main.styled";
 
 const RocketPage = () => {
   const [rocketData, setRocketData] = useState<Rocket | null>(null);
   const { id } = useParams();
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -29,12 +22,14 @@ const RocketPage = () => {
     fetchData();
   }, [id]);
 
-  return rocketData ? (
-    <Main>
-      <IntroSection rocketData={rocketData} />
-      <RocketDetails rocketData={rocketData} />
-  
-    </Main>
-  ) : null;
+  return (
+    rocketData && (
+      <Main>
+        <IntroSection rocketData={rocketData} />
+        <RocketDetails rocketData={rocketData} />
+      </Main>
+    )
+  );
 };
+
 export default RocketPage;
