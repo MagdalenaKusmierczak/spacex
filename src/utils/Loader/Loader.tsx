98372c27668464b400f9ffa6de120d84704f4cb1
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-12 09:26:07 +0200

    clearing code
    delete depricated code
    
    clarity of code -spaces

--- src/utils/Loader/Loader.tsx
+++ src/utils/Loader/Loader.tsx
@@ -15,4 +15,5 @@ const Loader = () => {
     </LoaderWrapper>
   );
 };
+
 export default Loader;
--- src/utils/setParagraph.tsx
+++ src/utils/setParagraph.tsx
@@ -2,9 +2,7 @@ import {
   StyledParagraph,
   StyledSpan,
 } from "../components/RocketSections/RocketSections.styled";
-import { ReactNode } from "react";
 
-//Setting default paragraph for data
 const setParagraph = (
   name: string,
   data: number | string | boolean | null,
@@ -20,4 +18,5 @@ const setParagraph = (
     </StyledParagraph>
   );
 };
+
 export default setParagraph;
