84d22c108c550a0e0d484ee886ca9b564f148862
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-13 10:01:04 +0200

    Update ButtonsList.tsx
    clearing code
    fixing ids

--- src/components/ButtonsList/ButtonsList.tsx
+++ src/components/ButtonsList/ButtonsList.tsx
@@ -19,14 +19,16 @@ const ButtonsList = ({ handleClick, activeSection }: ButtonsListProps) => {
     <MenuWrapper>
       <MenuButton data-target="details" onClick={handleToggle}>
         <SectionName>
-          {activeSection ? `Details: ${activeSection}` : "Details"}
+          {activeSection
+            ? `Details: ${activeSection.replace("-", " ")}`
+            : "Details"}
         </SectionName>
         {isOpen && (
           <ButtonsWrapper>
             <Button data-target="gallery" onClick={handleClick}>
               <SectionName>Gallery</SectionName>
             </Button>
-            <Button data-target="general information" onClick={handleClick}>
+            <Button data-target="general-information" onClick={handleClick}>
               <SectionName>General information</SectionName>
             </Button>
             <Button data-target="dimensions" onClick={handleClick}>
@@ -35,10 +37,10 @@ const ButtonsList = ({ handleClick, activeSection }: ButtonsListProps) => {
             <Button data-target="engines" onClick={handleClick}>
               <SectionName>Engines</SectionName>
             </Button>
-            <Button data-target="landing legs" onClick={handleClick}>
+            <Button data-target="landing-legs" onClick={handleClick}>
               <SectionName>Landing legs</SectionName>
             </Button>
-            <Button data-target="payloads" onClick={handleClick}>
+            <Button data-target="payloads-weights" onClick={handleClick}>
               <SectionName>Payloads weights</SectionName>
             </Button>
             <Button data-target="stages" onClick={handleClick}>
