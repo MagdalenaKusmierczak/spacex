1b7f1b8d14ae0a66eecb0bfc4adfb75c96ca9c4b
Bartosz StingRay Polanczyk <bartosz.polanczyk@gmail.com>
2025-05-10 23:26:53 +0200

    Fixed one CSS issue.

--- src/components/AnimatedStart/AnimatedStars.styled.tsx
+++ src/components/AnimatedStart/AnimatedStars.styled.tsx
@@ -36,7 +36,7 @@ export const DefaultSpan = styled.span`
     0 0 0 8px rgba(255, 255, 255, 0.1),
     0 0 20px rgba(255, 255, 255, 0.1);
   animation: ${shooting} 5s linear infinite;
-  &: before {
+  &:before {
     content: "";
     position: absolute;
     top: 50%;
