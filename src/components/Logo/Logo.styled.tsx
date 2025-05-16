aa9b6be08f3a32978ab3a011932b482e52e93299
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2025-05-16 11:30:46 +0200

    Logo styles
    Changing colors

--- src/components/Logo/Logo.styled.tsx
+++ src/components/Logo/Logo.styled.tsx
@@ -1,19 +1,18 @@
 import styled from "styled-components";
 
 export const ImageWrapper = styled.div`
-  display: flex;
+  display: flex; 
   align-items: center;
   justify-content: center;
   width: 80px;
   height: 80px;
-  border-radius: 50px;
-  background-color: #f0ff3836;
+   background-color:rgb(216, 198, 154);
   box-shadow: 0px 0px 8px 8px #f0ff3836;
   border-radius: 50%; // Repeated border-radius
   transition: all 0.3s ease-in-out 0s;
     &:hover {
     transform: rotate(260deg);
-    background-color: #ffc41f;
-    box-shadow: 0px 0px 8px 8px #fada7e;}
+    border-color: #ffc41f;
+    box-shadow: 0 0 10px 0 #ffc41f inset, 0 0 10px 4px #ffc41f;}
   }
 `;
